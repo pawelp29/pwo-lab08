d9ae296c70062b263883184e98cfc5e7f5dbce19
Paweł Pańczyk <pawel.panczyk@pollub.edu.pl>
2022-11-16 17:21:50 +0100

    Realizacja laboratorium 8

---
+++ lab08-engine/src/main/java/pwo/utils/SequenceTools.java
@@ -0,0 +1,55 @@
+package pwo.utils;
+
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class SequenceTools {
+
+    private static String getTerms(SequenceGenerator sg,
+            int from, int to, String sep) {
+        
+        boolean backwards = false;
+        if (from > to) {
+            backwards = true;
+            int tmp = from;
+            from = to;
+            to = tmp;
+        }
+
+        int i = from, stop = to, step = 1;
+        String terms = "";
+        while (true) {
+            if (backwards) {
+                terms = sg.getTerm(i) + sep + terms;
+            } else {
+                terms += sg.getTerm(i) + sep;
+            }
+            if (i == stop) {
+                return terms.trim();
+            }
+            i += step;
+        }
+    }
+
+    public static String getTermsAsColumn(SequenceGenerator sg,
+            int from, int to) {
+        return getTerms(sg, from, to, "\n");
+    }
+
+    public static String getTermsAsLine(SequenceGenerator sg,
+            int from, int to) {
+        return getTerms(sg, from, to, " ");
+    }
+
+    public static boolean writeToFile(SequenceGenerator sg,
+            int from, int to, String fileName) {
+        try ( BufferedWriter writer = new BufferedWriter(
+                new FileWriter(fileName))) {
+            writer.write(getTermsAsColumn(sg, from, to));
+        } catch (IOException ex) {
+            return false;
+        }
+        return true;
+    }
+}
